1dd8a96ffd7a8d1d48f9009b23dd75d7ce32557a
BigBob489 <joeymcboi21@gmail.com>
2026-02-15 23:59:46 -0500

    Update main.js
    Made it so that borders automatically go to the right color when you first reload the page.

--- ufo50tracker/main.js
+++ ufo50tracker/main.js
@@ -2,28 +2,28 @@ const borderArray = ["5px solid black", "5px solid yellow", "5px solid green"];
 
 
 let setCookie = function(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  let expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires;
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires;
 };
 
 
 let getCookie = function(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  console.log(decodedCookie)
-  let ca = decodedCookie.split(';');
-  for(let i = 0; i <ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    };
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    };
-  }
-  return "";
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    console.log(decodedCookie)
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        };
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        };
+    }
+    return "";
 };
 
 
@@ -37,6 +37,7 @@ let init = function() {
         img.setAttribute("data-completion", getCookie(img.id));
         img.setAttribute("onclick", "trackStuff(this);");
         img.setAttribute("draggable", "false");
+        me.style.border = borderArray[Number(me.getAttribute("data-completion"))];
         document.getElementById("tracker").appendChild(img);
     };
 
